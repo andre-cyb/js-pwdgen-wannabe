47f4c4ba33d1f619d3092fafe117bf6796352497
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-07 16:10:32 +0200

    esercizio JS completato

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,8 +4,8 @@ const promptFirstName = prompt("inserire il nome");
 const promptLastName = prompt("inserire il cognome");
 const promptFavouriteColor = prompt("inserire il colore preferito");
 
-console.log("risultato della richiesta:", promptFirstName);
-console.log("risultato della richiesta:", promptLastName);
-console.log("risultato della richiesta:", promptFavouriteColor);
+console.log("risultato della richiesta nome:", promptFirstName);
+console.log("risultato della richiesta cognome:", promptLastName);
+console.log("risultato della richiesta colore:", promptFavouriteColor);
 
 alert("Ciao! Dalle tue risposte il tuo nome è " + promptFirstName + " " + promptLastName + " ed il tuo colore preferito è il " + promptFavouriteColor + " " + 21);
